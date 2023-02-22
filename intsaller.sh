2e2d38b27ebf5a442a4a2e0ed482cab9dc4b81c4
bossofdevil <66734043+h4ckerharish@users.noreply.github.com>
2023-02-22 12:15:33 +0530

    Update intsaller.sh

--- intsaller.sh
+++ intsaller.sh
@@ -1,1 +1,19 @@
 pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is harish
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "\. ..Script By {h4ckerharish}.../"| lolcat
+apt-get update -y
+apt get-upgrade -y
+apt install python2 -y
+apt install wget -y
+apt install ruby -y
+apt install git -y
